5f0a56e652625359d893a2f86e56aebe36c49b8a
성당현 <1003dang@naver.com>
2023-11-02 22:32:15 +0900

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,31 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routers/home";
+import Profile from "./routers/profile";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "profile",
+        element: <Profile />
+      }
+    ]
+  }
+])
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App
