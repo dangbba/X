2089ad6f71afb2d8b6156e12f085bbe7ae04a996
성당현 <1003dang@naver.com>
2023-11-02 22:46:49 +0900

    login

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./components/layout";
 import Home from "./routers/home";
 import Profile from "./routers/profile";
+import Login from "./routers/login";
+import CreateAccount from "./routers/create-account";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +19,14 @@ const router = createBrowserRouter([
         element: <Profile />
       }
     ]
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />
   }
 ])
 
