b9ebf15a25beb06b2e811a7099fad3216e84f179
성당현 <1003dang@naver.com>
2023-11-06 17:09:58 +0900

    auth component

--- src/routers/create-account.tsx
+++ src/routers/create-account.tsx
@@ -1,50 +1,9 @@
-import styled from "styled-components";
 import { useState } from "react";
 import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth } from "../firebase";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FirebaseError } from "firebase/app";
-
-const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 420px;
-  padding: 50px 0px;
-`;
-
-const Title = styled.h1`
-  font-size: 42px;
-`;
-
-const Form = styled.form`
-  margin-top: 50px;
-  margin-bottom: 10px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-
-const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type="submit"] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`;
+import { Error, Form, Input, Switcher, Title, Wrapper } from "../components/auth-component";
 
 export default function CreateAccount(){
   const navigate = useNavigate();
@@ -93,6 +52,10 @@ export default function CreateAccount(){
         <Input onChange={onChange} type="submit" value={isLoading ? "Loading..." : "Create Account"} />
       </Form>
       {error !== '' ? <Error>{error}</Error> : null}
+      <Switcher>
+        Already have an account? {''}
+        <Link to="/login">Log in &rarr;</Link>
+      </Switcher>
     </Wrapper>
   );
 }
