b3d40325d0fad4ba84afcef16f494fb89b2c2459
성당현 <1003dang@naver.com>
2023-11-08 16:45:44 +0900

    logout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,5 +1,6 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import { auth } from "../firebase";
 
 const Wrapper = styled.div`
   display: grid;
@@ -40,6 +41,14 @@ const MenuItem = styled.div`
 `;
 
 export default function Layout(){
+  const navigate = useNavigate();
+  const onLogOut = async() => {
+    const ok = confirm("Are you sure you want to log out?");
+    if(ok){
+      await auth.signOut();
+      navigate("/login");
+    } 
+  }
   return (
     <Wrapper>
       <Menu>
@@ -57,7 +66,7 @@ export default function Layout(){
             </svg>
           </MenuItem>
         </Link>
-        <MenuItem className="log-out">
+        <MenuItem onClick={onLogOut} className="log-out">
           <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
             <path clipRule="evenodd" fillRule="evenodd" d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z" />
             <path clipRule="evenodd" fillRule="evenodd" d="M19 10a.75.75 0 00-.75-.75H8.704l1.048-.943a.75.75 0 10-1.004-1.114l-2.5 2.25a.75.75 0 000 1.114l2.5 2.25a.75.75 0 101.004-1.114l-1.048-.943h9.546A.75.75 0 0019 10z" />
